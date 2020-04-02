c5e4383ba051b0307e966bf7cd9c96771d80000a
YAMMEN10 <YAMMEN.githup98@gmail.com>
2020-04-03 02:08:40 +0300

    finish login activity

--- app/src/main/java/com/example/whatsapp/Activities/LoginActivity/LoginActivity.java
+++ app/src/main/java/com/example/whatsapp/Activities/LoginActivity/LoginActivity.java
@@ -1,7 +1,10 @@
 package com.example.whatsapp.Activities.LoginActivity;
 
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.example.whatsapp.Firebase.$FirebaseController;
 import com.example.whatsapp.MainActivity;
@@ -15,7 +18,10 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT){
+            Window w = getWindow();
+            w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        }
         this.firebase_controller = new $FirebaseController();
 
     }
@@ -24,8 +30,8 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        if (this.firebase_controller.getFirebase_user() == null) {
-            $Utils.go_to_target_activity(LoginActivity.this, MainActivity.class);
-        }
+//        if (this.firebase_controller.getFirebase_user() == null) {
+//            $Utils.go_to_target_activity(LoginActivity.this, MainActivity.class);
+//        }
     }
 }
