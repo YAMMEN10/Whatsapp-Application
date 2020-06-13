038beeac5222d2317cc7c2e7a12fc62e812ca5c7
YAMMEN10 <YAMMEN.githup98@gmail.com>
2020-06-13 19:01:15 +0300

    create private chat activity

---
+++ app/src/main/java/com/example/whatsapp/ui/Activitys/PrivateChatActivity.java
@@ -0,0 +1,16 @@
+package com.example.whatsapp.ui.Activitys;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.whatsapp.R;
+
+public class PrivateChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_private_chat);
+    }
+}
